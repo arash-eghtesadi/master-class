0f3e2ef1590ab4b8a2bffdc7b3402b89091b859e
arash Eghtesadi <arash_eg@yahoo.com>
2017-10-23 18:11:54 -0700

    initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public static void main(String[] args){
+    boolean gameOver = true;
+    int score = 800;
+    int levelCompleted = 5;
+    int bonus = 100;
+
+    calculateScore();
+}
+
+public static void calculateScore(){
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+        }
